e9b0f0ace484f4b8cdca6346e3433933b9e41e14
yuvalbenshitrit <yuval056@gmail.com>
2025-01-29 17:47:22 +0200

    edit the addpost and add componen losfing

---
+++ src/components/Loading.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const Loading: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <CircularProgress />
+      <Typography variant="h6" sx={{ mt: 2 }}>
+        Loading...
+      </Typography>
+    </Box>
+  );
+};
+
+export default Loading;
